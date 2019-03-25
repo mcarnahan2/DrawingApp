7567d91a6da75326f78ebabd2e91d6b38ec7bb27
mcarn <mlcarnahan22@gmail.com>
2019-03-24 21:41:40 -0500

    Added code to allow the image selected to be from the phone's gallery.

--- app/src/main/java/edu/apsu/drawingapp/MainActivity.java
+++ app/src/main/java/edu/apsu/drawingapp/MainActivity.java
@@ -1,13 +1,60 @@
 package edu.apsu.drawingapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView targetImage;
+    TextView textTargetUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textTargetUri = findViewById(R.id.textView);
+        targetImage = findViewById(R.id.imageView2);
+        Button button = findViewById(R.id.picture_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            textTargetUri.setText(targetUri.toString());
+
+            Bitmap bitmap;
+
+            try{
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+
+
+
+        }
     }
 }
