745febbb80351c92a6626ba3bf0608ef022c7d09
Keenya22 <keenyagilchrist@hotmail.com>
2019-04-04 22:58:33 -0500

    Edit MainActivty and mainactivity.xml

--- app/src/main/java/edu/apsu/drawingapp/MainActivity.java
+++ app/src/main/java/edu/apsu/drawingapp/MainActivity.java
@@ -4,15 +4,22 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.FileNotFoundException;
@@ -20,6 +27,11 @@ import java.io.FileNotFoundException;
 public class MainActivity extends AppCompatActivity {
 
     DrawingView drawingView;
+
+    private AlertDialog dialog;
+    private View dialogView;
+    private SeekBar sbwidth;
+    private int paintWidth;
     TextView textTargetUri;
     public static Bitmap bitmap;
     public static int backgroundColor = 0;
@@ -31,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
 
         textTargetUri = findViewById(R.id.textView);
         drawingView = findViewById(R.id.drawingView);
+        sbwidth = findViewById(R.id.seekBar1);
+        sbwidth.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                paintWidth = i+1;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         Button pictureButton = findViewById(R.id.picture_button);
         pictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +69,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button paintButton = findViewById(R.id.paint_button);
-        paintButton.setOnClickListener(new View.OnClickListener() {
+        ImageButton fillButton = findViewById(R.id.fill_imageButton6);
+        fillButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 backgroundColor = Color.BLUE;
+            }
+        });
+
+        ImageButton colorButton = findViewById(R.id.color_imageButton3);
+
+        final PopupMenu popupMenu = new PopupMenu(getApplicationContext(), colorButton);
+        final Menu menu = popupMenu.getMenu();
+        popupMenu.getMenuInflater().inflate(R.menu.color_menu, menu );
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.red) {
+                    drawingView.setBackgroundDrawable(new ColorDrawable(
+                            Color.RED));
+                } else if (menuItem.getItemId() == R.id.blue) {
+                    drawingView.setBackgroundDrawable(new ColorDrawable(
+                            Color.BLUE));
+                }
+                return false;
+            }
+        });
+        colorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popupMenu.show();
             }
         });
 
@@ -69,4 +124,5 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
 }
