8a77b9e94f6734b71cc8fb7073d4b9f9938b1b2b
mcarn <mlcarnahan22@gmail.com>
2019-04-08 09:04:10 -0500

    Rectangle and Circle imported

--- app/src/main/java/edu/apsu/drawingapp/DrawingView.java
+++ app/src/main/java/edu/apsu/drawingapp/DrawingView.java
@@ -8,19 +8,23 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
 import static edu.apsu.drawingapp.MainActivity.backgroundColor;
-import static edu.apsu.drawingapp.MainActivity.numbersSet;
+import static edu.apsu.drawingapp.MainActivity.buttonPressed;
 
 public class DrawingView extends View {
     Paint backgroundPaint;
     Paint rectanglePaint;
     Paint circlePaint;
+    Paint textPaint;
     private Paint paint;
     private Paint BitmapPaint;
     private Path path;
@@ -30,6 +34,21 @@ public class DrawingView extends View {
 
     private float pX;
     private float pY;
+
+    float rBeginX;
+    float rEndX;
+    float rBeginY;
+    float rEndY;
+    private RectF rect;
+
+    float cBeginX;
+    float cEndX;
+    float cBeginY;
+    float cEndY;
+    private RectF circ;
+
+    float tBeginX;
+    float tBeginY;
 
     private int paintColor = Color.BLACK;
     private static Paint.Style paintStyle = Paint.Style.STROKE;
@@ -52,8 +71,8 @@ public class DrawingView extends View {
 
     private void init() {
         backgroundPaint = new Paint();
-       // backgroundPaint.setColor(Color.RED);
-        //backgroundPaint.setStyle(Paint.Style.FILL);
+        backgroundPaint.setColor(Color.RED);
+        backgroundPaint.setStyle(Paint.Style.FILL);
         cacheBitmap = Bitmap.createBitmap(currentWidth, currentHeight, Bitmap.Config.ARGB_8888);
         cacheCanvas = new Canvas(cacheBitmap);
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -65,6 +84,22 @@ public class DrawingView extends View {
         rectanglePaint.setColor(Color.BLACK);
         rectanglePaint.setStyle(Paint.Style.FILL);
 
+        circlePaint = new Paint();
+        circlePaint.setColor(Color.BLACK);
+        circlePaint.setStyle(Paint.Style.FILL);
+
+        textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setStyle(Paint.Style.FILL);
+
+        textPaint.setAntiAlias(true);
+        textPaint.setTextAlign(Paint.Align.LEFT);
+
+        float spSize = 30; //30 sp
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        float pixelSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spSize, dm);
+
+        textPaint.setTextSize(pixelSize);
     }
 
     private void updatePaint() {
@@ -88,10 +123,6 @@ public class DrawingView extends View {
             MainActivity.backgroundColor = 0;
         } else {
             canvas.drawPaint(backgroundPaint);
-        }
-
-        if(MainActivity.numbersSet == 1){
-            canvas.drawRect(MainActivity.rectWidth, MainActivity.rectHeight, MainActivity.rectWidth, MainActivity.rectHeight, rectanglePaint);
         }
 
         if (clearScreen) {
@@ -119,15 +150,42 @@ public class DrawingView extends View {
                 path.moveTo(event.getX(), event.getY());
                 pX = event.getX();
                 pY = event.getY();
+
+                if(MainActivity.buttonPressed==2){
+                    rBeginX=pX;
+                    rBeginY=pY;
+                } else if(MainActivity.buttonPressed ==3){
+                    cBeginX=pX;
+                    cBeginY=pY;
+                }
+
                 break;
             case MotionEvent.ACTION_MOVE:
-                path.quadTo(pX, pY,event.getX(), event.getY());
+                if(MainActivity.buttonPressed == 1) {
+                    path.quadTo(pX, pY, event.getX(), event.getY());
+                }
                 pX = event.getX();
                 pY = event.getY();
                 break;
             case MotionEvent.ACTION_UP:
-                cacheCanvas.drawPath(path, paint);
-                path.reset();
+                if(buttonPressed==1) {
+                    cacheCanvas.drawPath(path, paint);
+                    path.reset();
+                } else if(buttonPressed==2) {
+                    rEndX = event.getX();
+                    rEndY = event.getY();
+                    rect = new RectF(rBeginX, rBeginY, rEndX, rEndY);
+                    cacheCanvas.drawRect(rect, rectanglePaint);
+                } else if(buttonPressed==3){
+                    cEndX =event.getX();
+                    cEndY = event.getY();
+                    circ = new RectF(cBeginX, cBeginY, cEndX, cEndY);
+                    cacheCanvas.drawOval(circ, circlePaint);
+                } else if(buttonPressed==4){
+                    tBeginX = event.getX();
+                    tBeginY = event.getY();
+                    cacheCanvas.drawText(MainActivity.text, tBeginX, tBeginY, textPaint);
+                }
                 break;
         }
         invalidate();
